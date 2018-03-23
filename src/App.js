36dcfde7e7c6d202ea0a953675ef9bdfe34c1657
Guilherme Brambila <gb.brambila@gmail.com>
2018-03-23 20:09:47 +1100

    Created WeatherComponent.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
+import WeatherComponent from './components/weather-component/WeatherComponent';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Weather Forecast Challenge
-      </div>
+      <WeatherComponent />
     );
   }
 }
---
+++ src/components/weather-component/WeatherComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class WeatherComponent extends Component {
+    render() {
+        return(
+            <div class="weather-component">
+                weather-component
+            </div>
+        )
+    }
+}
+
+export default WeatherComponent;
