e92734f7d338dedab0ee25c0a69931cb1086775b
Guilherme Brambila <gb.brambila@gmail.com>
2018-03-23 19:59:29 +1100

    Added first files to start the project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Weather Forecast Challenge
+      </div>
+    );
+  }
+}
+
+export default App;
