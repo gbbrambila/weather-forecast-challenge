77b05a125474dbd4731bd70a5ccac3541f87992d
Guilherme Brambila <gb.brambila@gmail.com>
2018-03-23 20:45:58 +1100

    Added WeatherComponent.scss

--- src/components/weather-component/WeatherComponent.js
+++ src/components/weather-component/WeatherComponent.js
@@ -3,6 +3,7 @@ import SearchBarComponent from './search-bar/SearchBarComponent'
 import CurrentLocationComponent from './current-location/CurrentLocationComponent'
 import CurrentWeatherComponent from './current-weather/CurrentWeatherComponent'
 import WeatherForecastListComponent from './weather-forecast/WeatherForecastListComponent'
+import './WeatherComponent.scss'
 
 class WeatherComponent extends Component {
     render() {
