b13af9b4f9056806ff57dc8364912276657b9b34
Guilherme Brambila <gb.brambila@gmail.com>
2018-03-26 01:37:46 +1100

    Changed current weather and current location style

--- src/components/weather-component/current-weather/CurrentWeatherComponent.js
+++ src/components/weather-component/current-weather/CurrentWeatherComponent.js
@@ -5,8 +5,8 @@ class CurrentWeatherComponent extends Component {
     render() {
         return(
             <div className="current-weather">
-                <h3 className="current-weather__date"> { this.props.currentWeather.date } </h3>
                 <h2 className="current-weather__temp"> { this.props.currentWeather.temp } </h2>
+                <h3 className="current-weather__date"> { this.props.currentWeather.date } </h3>
             </div>
         )
     }
